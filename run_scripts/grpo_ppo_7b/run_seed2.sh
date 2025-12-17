3412244525167ee88d70a33501500b5e29792856
Jiangweizhi Peng <peng0504@ahl01.agate.msi.umn.edu>
2025-12-17 01:03:41 -0600

    scripts

--- run_scripts/grpo_ppo_7b/run_seed2.sh
+++ run_scripts/grpo_ppo_7b/run_seed2.sh
@@ -8,7 +8,8 @@ SCRIPT_PPO="run_scripts/grpo_ppo_7b/qwen_ppo_7b.sh"
 SCRIPT_GRPO="run_scripts/grpo_ppo_7b/qwen_grpo_7b.sh"
 
 # Run A
-GPUS="0,1,2,3"
+GPUS_A="0,1,2,3"
+GPUS_B="4,5,6,7"
 SEED=2
 
 tmux has-session -t $SESSION 2>/dev/null && tmux kill-session -t $SESSION
@@ -17,13 +18,13 @@ tmux has-session -t $SESSION 2>/dev/null && tmux kill-session -t $SESSION
 tmux new-session -d -s "$SESSION" -n "PPO_seed${SEED}"
 
 tmux send-keys -t "$SESSION:PPO_seed${SEED}" \
-  "CUDA_VISIBLE_DEVICES=${GPUS} bash ${SCRIPT_PPO} ${ENGINE} ${SEED}" C-m
+  "CUDA_VISIBLE_DEVICES=${GPUS_A} bash ${SCRIPT_PPO} ${ENGINE} ${SEED}" C-m
 
 # second window
 tmux new-window -t "$SESSION" -n "GRPO_seed${SEED}"
 
 tmux send-keys -t "$SESSION:GRPO_seed${SEED}" \
-  "CUDA_VISIBLE_DEVICES=${GPUS} bash ${SCRIPT_GRPO} ${ENGINE} ${SEED}" C-m
+  "CUDA_VISIBLE_DEVICES=${GPUS_B} bash ${SCRIPT_GRPO} ${ENGINE} ${SEED}" C-m
 
 # detach
 # tmux detach -s "$SESSION"
