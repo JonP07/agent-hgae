27f05696f6bae623cf4af657fdbc34b5f6c2614d
Jiangweizhi Peng <peng0504@e6.agate.msi.umn.edu>
2025-12-19 17:13:36 -0600

    scripts

--- run_scripts/hgae/run.sh
+++ run_scripts/hgae/run.sh
@@ -12,17 +12,17 @@ SEED_A=1
 GPUS_A="0,1,2,3"
 GPUS_B="4,5,6,7"
 
-tmux has-session -t $SESSION 2>/dev/null && tmux kill-session -t $SESSION
+tmux has-session -t $SESSION 2>/dev/null && tmux  kill-session -t $SESSION
 
 # create session + first window
-tmux new-session -d -s "$SESSION" -n "seed${SEED_A}"
-  tmux send-keys -t "$SESSION:seed${SEED_A}" \
+tmux new-session -d -s "$SESSION" -n "3B"
+  tmux send-keys -t "$SESSION:3B" \
   "CUDA_VISIBLE_DEVICES=${GPUS_A} bash ${SCRIPT_HGAE_3B} ${ENGINE} ${SEED_A}" C-m
 
 # second window
-tmux new-window -t "$SESSION" -n "seed${SEED_B}"
+tmux new-window -t "$SESSION" -n "7B"
 
-tmux send-keys -t "$SESSION:seed${SEED_B}" \
+tmux send-keys -t "$SESSION:7B" \
   "CUDA_VISIBLE_DEVICES=${GPUS_B} bash ${SCRIPT_HGAE_7B} ${ENGINE} ${SEED_A}" C-m
 
 # detach
